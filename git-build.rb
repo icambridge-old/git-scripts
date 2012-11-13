993c38eb3b454262a1bd16972d051e1f369ccea6
Iain Cambridge <github@icambridge.me>
2012-11-13 22:17:00 +0000

    Added validation to git-build.rb
    jenkins.url and jenkins.token are required configuration options
    
    jenkins.job, jenkins.username, and jenkins.password are optional configuration options.

--- git-build.rb
+++ git-build.rb
@@ -6,20 +6,61 @@
 # License : MIT
 ##########################
 
-
-
 require 'net/http'
 require 'json'
 require 'git'
 
-if ARGV[0].nil? then
-  puts "A project name is required"
+#
+# @param [String] config_name
+#   The name of the configuration file that isn't found but is required.
+#
+def config_not_found(config_name)
+  puts "Error \"" + config_name+ "\" isn't configured properly"
   exit
 end
 
-g = Git.open(Dir.pwd)
+
+if ARGV[0].nil? == false and ARGV[0].downcase == '-h'  then
+  puts "Usage : git build [project_name]"
+  puts "\n"
+  puts "Config:"
+  puts "\tjenkins.url      : The URL Jenkins is available on"
+  puts "\tjenkins.token    : The token for the remote build trigger"
+  puts "Optional options:"
+  puts "\tjenkins.username : Your jenkins username"
+  puts "\tjenkins.password : Your jenkins password"
+  puts "\tjenkins.job      : The jenkins job that is to be build by default"
+  exit
+end
+
+
+begin
+  g = Git.open(Dir.pwd)
+rescue
+  puts "Fatal error git directory not found"
+  exit
+end
 
 config =  g.config
+
+if config['jenkins.url'].nil? then
+  config_not_found("jenkins.url")
+end
+
+if config['jenkins.token'].nil? then
+  config_not_found("jenkins.url")
+end
+
+if config['jenkins.job'].nil? and ARGV[0].nil? then
+  config_not_found("jenkins.url")
+end
+
+if config['jenkins.job'].nil? then
+  project_name  = ARGV[0]
+else
+  project_name  = config['jenkins.job']
+end
+
 
 uri = URI.parse(config['jenkins.url']+"api/json")
 
@@ -34,7 +75,6 @@ end
 response = http.request(request)
 json = JSON.parse(response.body)
 project_url = nil
-project_name  = ARGV[0]
 
 json['jobs'].each do |job|
   if job['name'].downcase == project_name.downcase then
@@ -51,6 +91,10 @@ end
 buildUri = URI.parse(project_url + 'build/?token=' + config['jenkins.token'])
 http = Net::HTTP.new(buildUri.host, buildUri.port)
 request = Net::HTTP::Get.new(buildUri.request_uri)
-request.basic_auth(config['jenkins.username'], config['jenkins.password'])
+
+if config['jenkins.username'].nil? then
+  request.basic_auth(config['jenkins.username'], config['jenkins.password'])
+end
+
 response = http.request(request)
 puts "Building " + project_name
