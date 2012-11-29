95b15d3c26de81a043083af27e69592586f9cc0c
Iain Cambridge <iain@icelolly.com>
2012-11-29 13:21:30 +0000

    Added safepush and safepull

---
+++ git-safepull.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby1.9.1
+
+require 'git'
+
+begin
+  git = Git.open(Dir.pwd)
+rescue
+  puts "Fatal error git directory not found"
+  exit
+end
+
+if ARGV[0].nil?
+  puts "Need remote"
+  exit
+end
+
+remote = ARGV[0]
+
+remotes = []
+git.remotes.map do |repo|
+   remotes.push repo.name
+end
+
+if !remotes.include?(remote)
+  puts "Invalid remote repository"
+  exit
+end
+
+current_branch = git.current_branch
+
+puts git.pull(remote, current_branch)
